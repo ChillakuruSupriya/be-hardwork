a56555c4074ae9118b504e3a9cf81d7aea3127f1
chillakurusupriya <chillakurusupriya@gmail.com>
2024-07-02 09:17:42 +0530

    new one

--- src/BeHappyForyourhappiness/MotiveYourSelf.java
+++ src/BeHappyForyourhappiness/MotiveYourSelf.java
@@ -6,6 +6,8 @@ public class MotiveYourSelf
 	public static void main(String[] args) 
 	{
 		System.out.println("everyone mindset is not same because of hand fingers are not equal");
+		
+		System.out.println("done");
 
 	}
 

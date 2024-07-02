b87bd1b174c22b76c4190149db1d3056b3710568
chillakurusupriya <chillakurusupriya@gmail.com>
2024-07-02 08:58:30 +0530

    stay loyal but not stupid

---
+++ src/BeHappyForyourhappiness/MotiveYourSelf.java
@@ -0,0 +1,12 @@
+package BeHappyForyourhappiness;
+
+public class MotiveYourSelf 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("everyone mindset is not same because of hand fingers are not equal");
+
+	}
+
+}
